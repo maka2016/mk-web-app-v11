99af03474a129abf63c5045429411d7c7db3c464
linli <linli@maka.im>
2025-11-06 16:42:33 +0800

    改loadingui和后门跳转

--- packages/jiantie/app/mobile/channel2/[id]/components/detail.tsx
+++ packages/jiantie/app/mobile/channel2/[id]/components/detail.tsx
@@ -93,7 +93,7 @@ export default function Detail({ channelId }: DetailProps) {
     return (
       <div className='flex items-center justify-center h-dvh bg-gray-50'>
         <div className='text-center'>
-          <div className='w-12 h-12 border-4 border-pink-600 border-t-transparent rounded-full animate-spin mx-auto mb-4'></div>
+          <div className='w-12 h-12 border-4 border-[#D53933] border-t-transparent rounded-full animate-spin mx-auto mb-4'></div>
           <p className='text-gray-500'>加载中...</p>
         </div>
       </div>
--- packages/jiantie/app/mobile/channel2/collection/[collectionId]/components/templates.tsx
+++ packages/jiantie/app/mobile/channel2/collection/[collectionId]/components/templates.tsx
@@ -88,7 +88,7 @@ export default function Templates({ collectionId }: TemplatesProps) {
     return (
       <div className='flex items-center justify-center h-dvh bg-white'>
         <div className='text-center'>
-          <div className='w-12 h-12 border-4 border-pink-600 border-t-transparent rounded-full animate-spin mx-auto mb-4'></div>
+          <div className='w-12 h-12 border-4 border-[#D53933] border-t-transparent rounded-full animate-spin mx-auto mb-4'></div>
           <p className='text-gray-500'>加载中...</p>
         </div>
       </div>
